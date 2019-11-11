76c4e2b390a06b022f414cd8ff68c245e6803aef
jratanar <jratanar@uwaterloo.ca>
2019-11-10 22:53:32 -0500

    Added player animation and Metal Blocks mechanic

--- FedoraField/Assets/Player.cs
+++ FedoraField/Assets/Player.cs
@@ -14,6 +14,10 @@ public class Player : MonoBehaviour
     public static int fieldState;
 
     private bool facingRight;
+    private bool facingLeft;
+
+    private int moveLeft;
+    private int moveRight;
     
     // Use this for initialization
     void Start()
@@ -25,6 +29,7 @@ public class Player : MonoBehaviour
         playerCollider = GetComponent<Collider2D>();
 
         facingRight = true;
+        facingLeft = false;
         speed = 4.0f;
 
         fieldState = 0;
@@ -47,8 +52,27 @@ public class Player : MonoBehaviour
             fieldState = 1;
         }
 
-            int moveLeft = Input.GetKey(KeyCode.A) ? 1 : 0;
-        int moveRight = Input.GetKey(KeyCode.D) ? 1 : 0;
+        //int moveLeft = Input.GetKey(KeyCode.A) ? 1 : 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveLeft = 1;
+            facingLeft = true;
+            facingRight = false;
+        } else
+        {
+            moveLeft = 0;
+        }
+        //int moveRight = Input.GetKey(KeyCode.D) ? 1 : 0;
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveRight = 1;
+            facingLeft = false;
+            facingRight = true;
+        }
+        else
+        {
+            moveRight = 0;
+        }
         int moveUp = Input.GetKey(KeyCode.W) ? 1 : 0;
         int moveDown = Input.GetKey(KeyCode.S) ? 1 : 0;
         float speedMultiplier = 1;
@@ -64,6 +88,18 @@ public class Player : MonoBehaviour
         }
         transform.position += Vector3.right * (moveRight - moveLeft) * speedMultiplier * speed * Time.deltaTime;
         transform.position += Vector3.up * (moveUp - moveDown) * speedMultiplier * speed * Time.deltaTime;
+
+
+        if (facingLeft && !facingRight)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+            facingLeft = false;
+        } else if (!facingLeft && facingRight)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            facingRight = false;
+        }
+
     }
 
     private void Flip(int scale) // Flip the player facing direction
